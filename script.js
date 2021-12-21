ce3b03d309aaac71cbdccba890418659c0f49025
Anurag Tawaniya <anuragtawaniya99@gmail.com>
2021-12-21 20:48:21 +0530

    new version

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-var loader = document.querySelector(".loader")
+let about = document.querySelector("#about");
+let portfoio = document.querySelector("#portfolio");
+let contact = document.querySelector("#contact");
 
-window.addEventListener("load",vanish)
+// change the content on clcik
+let content = document.querySelector(".about");
 
 
-function vanish(){
-    loader.classList.add("disppear");
-}
+about.addEventListener('click', () => {
+    content.innerHTML = `     
+    <section class="about">
+    <div class="heading">
+      <h1>About Me</h1>
+    </div>
+    <p>
+      Hello This is Anurag.I have always been enthusiastic about
+      computer &#128187; and that's why I chose this as my career. At first, what brought me closer to computers
+      were games.
+      But after that, I became much more interested in knowing how the web programs are created and how things work
+      behind the scene.I &#10084;&#65039; to build web and mobile apps.</p>
+  </section>`;
+})
+
+portfoio.addEventListener('click', () => {
+    content.innerHTML = `     
+    <div class="heading">
+    <h1>Portfolio</h1>
+  </div>
+ <p>Under the construction</p>
+</section>`;
+})
+
+contact.addEventListener('click', () => {
+    content.innerHTML = `     
+    <div class="heading">
+    <h1>Contact Me</h1>
+  </div>
+  <p>anuragtawaniya@gmail.com</p>
+</section>`;
+})
